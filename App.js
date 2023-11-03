97e51618fe5037cdab590d9e884211ce627ce3c7
Anthony Magpantay <anthony040190@yahoo.com>
2023-11-03 02:39:12 -0600

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+    SafeAreaView,
+    StatusBar
+} from 'react-native';
+import ToDoList from './todo-list';
+import ToDoForm from './todo-form';
+
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <SafeAreaView>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+            <ToDoList />
+
+            <ToDoForm />
+
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
+}
